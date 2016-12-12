acd5196cbb64827d6388609a2a5372ccb075a446
Kevin McCarthy <kjmagic13@gmail.com>
2016-12-12 17:14:09 -0500

    opportunity weights dependent of opportunities

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,6 +1,6 @@
 class Opportunity < ApplicationRecord
 
-	has_many :opportunity_weights, inverse_of: :opportunity, class_name: "AnswerOpportunity"
+	has_many :opportunity_weights, inverse_of: :opportunity, dependent: :destroy, class_name: "AnswerOpportunity"
 
 	validates :name, :link, presence: true
 	validates :link, format: URI::regexp(%w(http https))
